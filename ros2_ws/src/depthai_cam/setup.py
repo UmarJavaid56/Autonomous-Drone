c073596dbb279bbaf6fbb7c9e8345afefd1d1a12
Group7 <shraypatel@gmail.com>
2025-11-23 21:09:47 -0500

    OakD Mono + SLAM inital

---
+++ ros2_ws/src/depthai_cam/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'depthai_cam'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='group7',
+    maintainer_email='shraypatel@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'oak_publisher = depthai_cam.oak_publisher:main',
+        ],
+    },
+)
--- ros2_ws/src/orbslam3_bridge/src/orbslam3_mono_node.cpp
+++ ros2_ws/src/orbslam3_bridge/src/orbslam3_mono_node.cpp
@@ -1,92 +1,141 @@
 #include <memory>
 #include <string>
+#include <cmath>
 
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/image.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
 #include "cv_bridge/cv_bridge.h"
 #include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
-#include <System.h>   // ORB_SLAM3
+#include <System.h>           // ORB_SLAM3
+#include <sophus/se3.hpp>     // Sophus::SE3f
+#include <Eigen/Core>
+#include <Eigen/Geometry>
 
-using std::placeholders::_1;
-
-class OrbSlam3MonoNode : public rclcpp::Node
+class ORBMonoNode : public rclcpp::Node
 {
 public:
-  OrbSlam3MonoNode()
-  : Node("orbslam3_mono_node")
-  {
-    // Parameters for vocab + settings
-    this->declare_parameter<std::string>("voc_file", "/home/group7/Desktop/Autonomous-Drone/ros2_ws/src/external/ORB_SLAM3/Vocabulary/ORBvoc.txt");
-    this->declare_parameter<std::string>("settings_file", "/home/group7/Desktop/Autonomous-Drone/ros2_ws/src/external/ORB_SLAM3/Examples/Monocular/YourCamera.yaml");
-    this->declare_parameter<std::string>("image_topic", "/camera/image_raw");
+    ORBMonoNode(const rclcpp::NodeOptions & options = rclcpp::NodeOptions())
+    : Node("orbslam3_mono_node", options)
+    {
+        // Declare parameters
+        this->declare_parameter<std::string>("voc_file", "");
+        this->declare_parameter<std::string>("settings_file", "");
+        this->declare_parameter<std::string>("image_topic", "/camera/image_raw");
 
-    std::string voc_file, settings_file, image_topic;
-    this->get_parameter("voc_file", voc_file);
-    this->get_parameter("settings_file", settings_file);
-    this->get_parameter("image_topic", image_topic);
+        std::string voc_file      = this->get_parameter("voc_file").as_string();
+        std::string settings_file = this->get_parameter("settings_file").as_string();
+        image_topic_              = this->get_parameter("image_topic").as_string();
 
-    RCLCPP_INFO(get_logger(), "ORB-SLAM3 vocab: %s", voc_file.c_str());
-    RCLCPP_INFO(get_logger(), "ORB-SLAM3 settings: %s", settings_file.c_str());
-    RCLCPP_INFO(get_logger(), "Subscribing to: %s", image_topic.c_str());
+        if (voc_file.empty() || settings_file.empty()) {
+            RCLCPP_FATAL(
+                this->get_logger(),
+                "Parameters 'voc_file' and 'settings_file' must be set");
+            rclcpp::shutdown();
+            return;
+        }
 
-    // Initialize ORB-SLAM3 system (monocular)
-    slam_ = std::make_unique<ORB_SLAM3::System>(
-      voc_file,
-      settings_file,
-      ORB_SLAM3::System::MONOCULAR,
-      true    // use viewer
-    );
+        RCLCPP_INFO(
+            this->get_logger(),
+            "Starting ORB-SLAM3 MONO with voc: '%s', settings: '%s', topic: '%s'",
+            voc_file.c_str(), settings_file.c_str(), image_topic_.c_str());
 
-    // Subscribe to image topic
-    image_sub_ = this->create_subscription<sensor_msgs::msg::Image>(
-      image_topic,
-      10,
-      std::bind(&OrbSlam3MonoNode::imageCallback, this, _1)
-    );
-  }
+        // bUseViewer = false to avoid Pangolin/OpenGL on headless Jetson
+        slam_ = std::make_unique<ORB_SLAM3::System>(
+            voc_file, settings_file, ORB_SLAM3::System::MONOCULAR, false);
 
-  ~OrbSlam3MonoNode() override
-  {
-    if (slam_) {
-      slam_->Shutdown();
+        // Pose publisher
+        pose_pub_ = this->create_publisher<geometry_msgs::msg::PoseStamped>(
+            "/orbslam3/pose", 10);
+
+        // Subscribe to the image topic
+        sub_ = this->create_subscription<sensor_msgs::msg::Image>(
+            image_topic_, 10,
+            std::bind(&ORBMonoNode::imageCallback, this, std::placeholders::_1));
     }
-  }
+
+    ~ORBMonoNode() override
+    {
+        if (slam_) {
+            slam_->Shutdown();
+        }
+    }
 
 private:
-  void imageCallback(const sensor_msgs::msg::Image::SharedPtr msg)
-  {
-    cv_bridge::CvImageConstPtr cv_ptr;
-
-    try {
-      // Try to interpret as grayscale first
-      cv_ptr = cv_bridge::toCvShare(msg, sensor_msgs::image_encodings::MONO8);
-    } catch (cv_bridge::Exception &)
+    // Convert Sophus::SE3f Tcw (world -> camera) to Pose in world frame
+    bool TcwToPose(const Sophus::SE3f &Tcw, geometry_msgs::msg::Pose &pose_msg)
     {
-      // Fallback to color
-      try {
-        cv_ptr = cv_bridge::toCvShare(msg, sensor_msgs::image_encodings::BGR8);
-      } catch (cv_bridge::Exception &e) {
-        RCLCPP_ERROR(this->get_logger(), "cv_bridge exception: %s", e.what());
-        return;
-      }
+        // Rotation and translation in float
+        Eigen::Matrix3f Rcw = Tcw.so3().matrix();
+        Eigen::Vector3f tcw = Tcw.translation();
+
+        // Invert to get world->camera -> camera in world frame
+        Eigen::Matrix3f Rwc = Rcw.transpose();
+        Eigen::Vector3f twc = -Rwc * tcw;
+
+        // Rotation matrix -> quaternion
+        Eigen::Quaternionf q(Rwc);
+        q.normalize();
+
+        pose_msg.position.x = twc.x();
+        pose_msg.position.y = twc.y();
+        pose_msg.position.z = twc.z();
+
+        pose_msg.orientation.w = q.w();
+        pose_msg.orientation.x = q.x();
+        pose_msg.orientation.y = q.y();
+        pose_msg.orientation.z = q.z();
+
+        return true;
     }
 
-    double tframe = rclcpp::Time(msg->header.stamp).seconds();
+    void imageCallback(const sensor_msgs::msg::Image::ConstSharedPtr msg)
+    {
+        cv_bridge::CvImageConstPtr cv_ptr;
+        try {
+            // Our OAK publisher sends BGR8
+            cv_ptr = cv_bridge::toCvShare(msg, "bgr8");
+        } catch (cv_bridge::Exception &e) {
+            RCLCPP_ERROR(this->get_logger(), "cv_bridge exception: %s", e.what());
+            return;
+        }
 
-    slam_->TrackMonocular(cv_ptr->image, tframe);
-  }
+        cv::Mat imRGB;
+        cv::cvtColor(cv_ptr->image, imRGB, cv::COLOR_BGR2RGB);
 
-  std::unique_ptr<ORB_SLAM3::System> slam_;
-  rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr image_sub_;
+        double tframe = msg->header.stamp.sec +
+                        msg->header.stamp.nanosec * 1e-9;
+
+        // ORB-SLAM3 (newer) returns Sophus::SE3f
+        Sophus::SE3f Tcw = slam_->TrackMonocular(imRGB, tframe);
+
+        // (Optionally you could check tracking state here via slam_->GetTrackingState())
+
+        geometry_msgs::msg::PoseStamped pose_msg;
+        pose_msg.header.stamp = msg->header.stamp;
+        pose_msg.header.frame_id = "world";   // or "map"
+
+        if (!TcwToPose(Tcw, pose_msg.pose)) {
+            return;
+        }
+
+        pose_pub_->publish(pose_msg);
+    }
+
+    std::unique_ptr<ORB_SLAM3::System> slam_;
+    std::string image_topic_;
+    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr sub_;
+    rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr pose_pub_;
 };
 
 int main(int argc, char **argv)
 {
-  rclcpp::init(argc, argv);
-  auto node = std::make_shared<OrbSlam3MonoNode>();
-  rclcpp::spin(node);
-  rclcpp::shutdown();
-  return 0;
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<ORBMonoNode>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
 }
 
